4057b67809fa9f4d47c4bf08763cb32f91265c8e
Sahil Raje <sahilraje1111@gmail.com>
2026-07-30 13:26:42 +0530

    Initialize Java Fundamentals project with VariablesDemo

---
+++ 01-Java-Fundamentals/src/variables/VariablesDemo.java
@@ -0,0 +1,21 @@
+package variables;
+
+public class VariablesDemo {
+
+	public static void main(String[] args) {
+		int age;
+		String name;
+		double salary;
+		
+		age = 22;
+		name = "sahil";
+		salary = 100000;
+		
+		System.out.println(age);
+		System.out.println(name);
+		System.out.println(salary);
+		
+
+	}
+
+}
