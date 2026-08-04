0d5e21e3346db6975082a20fa0fe320afdf28ade
Sahil Raje <sahilraje1111@gmail.com>
2026-08-04 12:19:52 +0530

    Datatype Example Added

---
+++ 01-Java-Fundamentals/src/datatypes/Ex2.java
@@ -0,0 +1,33 @@
+package datatypes;
+
+public class Ex2 {
+	
+	
+	 public static void main(String[] args)
+	     {
+
+	        byte age = 22;
+	        short employeeId = 1050;
+	        int salary = 50000;
+	        long phoneNumber = 9876543210L;
+
+	        float height = 5.7f;
+	        double accountBalance = 125000.75;
+
+	        char grade = 'A';
+
+	        boolean isPermanentEmployee = true;
+
+	        System.out.println("Employee Details");
+	        System.out.println("-------------------------");
+	        System.out.println("Age: " + age);
+	        System.out.println("Employee ID: " + employeeId);
+	        System.out.println("Salary: ₹" + salary);
+	        System.out.println("Phone Number: " + phoneNumber);
+	        System.out.println("Height: " + height + " ft");
+	        System.out.println("Account Balance: ₹" + accountBalance);
+	        System.out.println("Performance Grade: " + grade);
+	        System.out.println("Permanent Employee: " + isPermanentEmployee);
+	    }
+
+}
